e9cdab28195c081e27766df8e8ff79337736a452
Marty Sama <martysama0134@users.noreply.github.com>
2020-05-09 22:39:25 +0200

    Update INIReaderTest.cpp

--- INIReaderTest.cpp
+++ INIReaderTest.cpp
@@ -18,7 +18,7 @@ int main()
     INIReader reader("test.ini");
 
     if (reader.ParseError() < 0)
-	{
+    {
         std::cout << "Can't load 'test.ini'\n";
         return 1;
     }
@@ -31,24 +31,24 @@ int main()
               << reader.GetReal("user", "pi", -1) << ", active="
               << reader.GetBoolean("user", "active", true) << "\n";
 
-	std::cout << reader.Get("config", "value", "UNK") << '\n';
+    std::cout << reader.Get("config", "value", "UNK") << '\n';
 
-	auto & sections = reader.Sections();
-	std::cout << sections.size() << '\n';
+    auto & sections = reader.Sections();
+    std::cout << sections.size() << '\n';
 
-	for (auto& section : sections)
-	{
-		std::cout << "section " << section << '\n';
-		for (auto& value : reader.Values(section))
-		{
-			auto res = reader.Get(section, value);
-			std::cout << "config[" << section << "][" << value << "] = " << res << '\n';
-		}
-	}
+    for (auto& section : sections)
+    {
+        std::cout << "section " << section << '\n';
+        for (auto& value : reader.Values(section))
+        {
+            auto res = reader.Get(section, value);
+            std::cout << "config[" << section << "][" << value << "] = " << res << '\n';
+        }
+    }
 
-	for (auto& value : reader.Values())
-	{
-		std::cout << "value " << value << '\n';
-	}
+    for (auto& value : reader.Values())
+    {
+        std::cout << "value " << value << '\n';
+    }
     return 0;
 }
